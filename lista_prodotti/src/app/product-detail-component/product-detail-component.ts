65d328054c4de60b72edd9a8c001c3c3ceeb2669
villanueva prince <villanueva.prince@iisgalvanimi.edu.it>
2026-04-23 20:07:20 +0200

    rifare da capo nuovamente

---
+++ lista_prodotti/src/app/product-detail-component/product-detail-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-detail-component',
+  imports: [],
+  templateUrl: './product-detail-component.html',
+  styleUrl: './product-detail-component.css',
+})
+export class ProductDetailComponent {}
---
+++ lista_prodotti/src/app/product-list-component/product-list-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list-component',
+  imports: [],
+  templateUrl: './product-list-component.html',
+  styleUrl: './product-list-component.css',
+})
+export class ProductListComponent {}
